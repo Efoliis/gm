9d050c32a481db8372a1c41bf0ca7d357c1898aa
Efoliis <79507270+Efoliis@users.noreply.github.com>
2021-03-22 12:37:48 +0400

    Add files via upload

---
+++ Clewas.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const request = require('request');
+
+let ayarlar = {
+    "özelurl": "url değişince yapılcak url",
+    "urlog": "botun mesaj atacağı guard log id",
+    "token": "botunuzun tokeni"
+}
+
+client.on('guildUpdate', async (nexis, aksoy) => {
+    if (nexis.vanityURLCode === aksoy.vanityURLCode) return;
+    let entry = await aksoy.fetchAuditLogs({
+        type: 'GUILD_UPDATE'
+    }).then(audit => audit.entries.first());
+    if (!entry.executor || entry.executor.id === client.user.id) return;
+    let channel = client.channels.cache.get(Options.urlog);
+
+    if (channel) channel.send(`
+    ${entry.executor} adlı kullanıcı özel url'yi değiştirmeye çalıştı ve sunucudan yasaklandı.
+    `)
+
+    if (!channel) aksoy.owner.send(`
+    ${entry.executor} adlı kullanıcı özel url'yi değiştirmeye çalıştı ve sunucudan yasaklandı.
+    `)
+
+    aksoy.members.ban(entry.executor.id, {
+        reason: `${entry.executor.tag} URL GUARD`
+    });
+
+    const ayarlar = {
+        url: `https://discord.com/api/v6/guilds/${aksoy.id}/vanity-url`,
+        body: { code: ayarlar.özelurl },
+        json: true,
+        method: 'PATCH',
+        headers: { "Authorization": `Bot ${ayarlar.token}` }
+    };
+
+    request(ayarlar, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gm
+# Url-guard
+
+Botu başlatmak için
+
+1- Clewas.js da koyduğum ayarları doldurun
+
+2- Komut sistemlerini çalıştırdıktan sonra bot kısmına npm i komutunu yazın.
+
+3- Herşey tamamlandıktan sonra node clewas.js yazarak botu başlatın.
